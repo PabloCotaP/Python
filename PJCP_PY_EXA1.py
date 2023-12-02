da8552eac9679ca0d0edbf240b9c952472072751
Pablo Cota <112607653+PabloCotaP@users.noreply.github.com>
2023-12-01 16:35:13 -0800

    Update PJCP_PY_EXA1.py

--- PJCP_PY_EXA1.py
+++ PJCP_PY_EXA1.py
@@ -14,3 +14,19 @@ while True:
         facto = factorial(num)
         print(f"El factorial de {num} es: {facto}")
         break
+    
+def area_trian(base, altura):
+    area = (base * altura)/2
+    
+    return area
+    
+while True:
+    try: 
+        b = int(input("\nIngrese la base del triangulo: "))
+        h = int(input("Ingrese la altura del triangulo: "))
+    except:
+        print("Alguno de los valores es invalido, intente de nuevo")
+    else:
+        area = area_trian(b, h)
+        print(f"El area de triangulo es: {area}")
+        break
